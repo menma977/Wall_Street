3c809bb84a3f89ee793712f14fa867625a57b9e4
SmityWaberJagerManJanson <com.owl.minerva@gmail.com>
2020-12-10 12:51:11 +0700

    add new data

---
+++ app/Http/Controllers/ETHController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ETHController extends Controller
+{
+    //
+}
---
+++ database/migrations/2020_12_10_054721_create_binaries_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBinariesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('binaries', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('binaries');
+    }
+}
