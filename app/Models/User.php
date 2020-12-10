9a81fe7eaf60a2c6a1300aed594ecd9531fd3783
SmityWaberJagerManJanson <com.owl.minerva@gmail.com>
2020-12-10 15:31:16 +0700

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Passport\HasApiTokens;
 
 /**
  * Class User
@@ -34,7 +35,7 @@ use Illuminate\Notifications\Notifiable;
  */
 class User extends Authenticatable
 {
-  use HasFactory, Notifiable, SoftDeletes;
+  use HasFactory, Notifiable, SoftDeletes, HasApiTokens;
 
   /**
    * The attributes that are mass assignable.
