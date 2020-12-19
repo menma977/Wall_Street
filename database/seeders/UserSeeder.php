af9d402e822ca648b85442348d4396277daea5dd
SmityWaberJagerManJanson <com.owl.minerva@gmail.com>
2020-12-19 13:26:37 +0700

    Update UserSeeder.php

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -44,12 +44,12 @@ class UserSeeder extends Seeder
     $user->password_junk = 'bank';
     $user->secondary_password = Hash::make('123456');
     $user->secondary_password_junk = '123456';
-    $user->username_doge = 'menma977';
-    $user->password_doge = '081211610807';
-    $user->wallet_btc = '1JWfKbLhkFfe9N7b4zVriHPpWXCAAMecdZ';
-    $user->wallet_ltc = 'LQQnR56oN7WHLsoiTj4RP9MaVb1u7rzAnp';
-    $user->wallet_doge = 'DM9L2mUkLwwbdoHMBjj5g2NLVXoVKPFyag';
-    $user->wallet_eth = '0xda6b7abb9830d11bbde456178f435e1a23ae4776';
+    $user->username_doge = 'bankwall';
+    $user->password_doge = '123456+A';
+    $user->wallet_btc = '1BNr4qg1b8tEmvVU1LPLqQZNcxkW7XERax';
+    $user->wallet_ltc = 'LVrhjRoAvQ6K6ACp1trXd6FV2nTZvkage4';
+    $user->wallet_doge = 'DNinFWZDow6KgHwgKtvoEkAKDZzq9JddqT';
+    $user->wallet_eth = '0xe48183f994292ace1d14301f84887747e4539434';
     $user->level = 0;
     $user->save();
   }
