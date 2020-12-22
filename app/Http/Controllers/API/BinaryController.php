38fa218476c41733e4ff81d12daa1b686dacf61b
SmityWaberJagerManJanson <com.owl.minerva@gmail.com>
2020-12-22 15:49:33 +0700

    Update BinaryController.php

--- app/Http/Controllers/API/BinaryController.php
+++ app/Http/Controllers/API/BinaryController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use App\Models\Binary;
+use App\Models\UpgradeList;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -16,6 +17,14 @@ class BinaryController extends Controller
     $binary = Binary::where('up_line', Auth::user()->id)->get();
     $binary->map(function ($item) {
       $item->userDownLine = User::find($item->down_line);
+      if ($item->userDownLine->level == 0) {
+        $dollar = 0;
+      } elseif ($item->userDownLine->level == 10) {
+        $dollar = 10000;
+      } else {
+        $dollar = UpgradeList::find($item->userDownLine->level)->dollar;
+      }
+      $item->userDownLine->level = "$" . number_format($dollar, 2, ',', '.');
 
       return $item;
     });
@@ -33,6 +42,12 @@ class BinaryController extends Controller
     $binary = Binary::where('up_line', $id)->get();
     $binary->map(function ($item) {
       $item->userDownLine = User::find($item->down_line);
+      if ($item->id == 1) {
+        $dollar = 10000;
+      } else {
+        $dollar = UpgradeList::find($item->userDownLine->level)->dollar;
+      }
+      $item->userDownLine->level = "$" . number_format($dollar, 2, ',', '.');
 
       return $item;
     });
