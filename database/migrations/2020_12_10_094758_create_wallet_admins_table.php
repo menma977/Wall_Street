5e712857fe02eb402a63dc2636ef93355bae7fd6
SmityWaberJagerManJanson <com.owl.minerva@gmail.com>
2020-12-26 14:32:25 +0700

    Update 2020_12_10_094758_create_wallet_admins_table.php

--- database/migrations/2020_12_10_094758_create_wallet_admins_table.php
+++ database/migrations/2020_12_10_094758_create_wallet_admins_table.php
@@ -21,7 +21,6 @@ class CreateWalletAdminsTable extends Migration
       $table->text('wallet_doge');
       $table->text('wallet_ltc');
       $table->text('wallet_eth');
-      $table->text('wallet_camel');
       $table->timestamps();
       $table->softDeletes();
     });
