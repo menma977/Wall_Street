e78113e01ba4a69ea709180aac451afc5b18b752
Smity Waber Jager Man Janson <38025704+menma977@users.noreply.github.com>
2021-01-14 21:26:55 +0700

    Update confirm-password.blade.php

--- resources/views/auth/confirm-password.blade.php
+++ resources/views/auth/confirm-password.blade.php
@@ -1,36 +1,40 @@
-<x-guest-layout>
-    <x-auth-card>
-        <x-slot name="logo">
-            <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-            </a>
-        </x-slot>
+@extends('layouts.guest')
 
-        <div class="mb-4 text-sm text-gray-600">
-            {{ __('This is a secure area of the application. Please confirm your password before continuing.') }}
-        </div>
+@section('content')
+  <div class="login-box">
+    <div class="login-logo">
+      <img src="{{ asset('logo.png') }}" alt="Wall Street" class="brand-image img-circle elevation-3" style="opacity: .8; width: 15%">
+      <a href="{{ route('welcome') }}"><b>Wall</b><small>Street</small></a>
+    </div>
 
-        <!-- Validation Errors -->
-        <x-auth-validation-errors class="mb-4" :errors="$errors" />
+    <div class="card">
+      <div class="card-body login-card-body">
+        <p class="login-box-msg">This is a secure area of the application.</p>
+        <p class="login-box-msg">Please confirm your password before continuing.</p>
 
-        <form method="POST" action="{{ route('password.confirm') }}">
-            @csrf
-
-            <!-- Password -->
-            <div>
-                <x-label for="password" :value="__('Password')" />
-
-                <x-input id="password" class="block mt-1 w-full"
-                                type="password"
-                                name="password"
-                                required autocomplete="current-password" />
+        <form action="{{ route('password.confirm') }}" method="post">
+          @csrf
+          <div class="input-group">
+            <input id="_password" name="password" type="password" class="form-control @error('password') is-invalid @enderror" placeholder="Password">
+            <div class="input-group-append">
+              <div class="input-group-text">
+                <label for="_password" class="fas fa-lock"></label>
+              </div>
             </div>
-
-            <div class="flex justify-end mt-4">
-                <x-button>
-                    {{ __('Confirm') }}
-                </x-button>
+          </div>
+          <div class="mb-3">
+            @error('password')
+            <small class="text-danger">{{ $message }}</small>
+            @enderror
+          </div>
+          <div class="row">
+            <div class="col-8"></div>
+            <div class="col-4">
+              <button type="submit" class="btn btn-primary btn-block">Confirm</button>
             </div>
+          </div>
         </form>
-    </x-auth-card>
-</x-guest-layout>
+      </div>
+    </div>
+  </div>
+@endsection
