eb2e186c509fbc242240861294477e5e28b7b75e
SmityWaberJagerManJanson <com.owl.minerva@gmail.com>
2021-01-16 17:39:05 +0700

    Update RouteServiceProvider.php

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -59,7 +59,7 @@ class RouteServiceProvider extends ServiceProvider
     RateLimiter::for('api', function (Request $request) {
       //return Limit::perMinute(60)->by(optional($request->user())->id ?: $request->ip());
       //return Limit::perMinute(30);
-      return Limit::perMinute(20)->response(function () {
+      return Limit::perMinute(10)->by(optional($request->user())->id ?: $request->ip())->response(function () {
         return response()->json(['message' => 'please slow down.'], 500);
       });
     });
